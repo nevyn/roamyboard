81349f4c46ad5db19f6c85b9e927f08e01b365f2
Nevyn Bengtsson <hello@nevyn.dev>
2026-01-03 00:29:44 +0100

    okay, curvature is happening!

--- case/roamy-v3.py
+++ case/roamy-v3.py
@@ -5,8 +5,8 @@ import math
 keys = 6
 key_pitch_y = 19.05
 
-col_w = 18.0
-col_h = 12 + (keys - 1) * key_pitch_y + 12
+col_w = 18.0 # width of the column module, viewed from the top
+col_h = 12 + (keys - 1) * key_pitch_y + 12 # height of the column module, viewed from the top
 thickness = 12.0 # in mm of the whole column module
 
 column_angle_deg = 8.0  # curvature per module (enforced by join geometry)
@@ -25,14 +25,15 @@ join_indent = 1.0
 join_clearance = 0.7 # how many mm of clearance to add to the tsocket to fit the groove
 
 # -------- Build straight shell --------
+# outer shell
 outer = cq.Workplane("XY").box(col_w, col_h, thickness, centered=True)
-
+# inner shell
 inner = (
     cq.Workplane("XY")
     .box(col_w - 2*wall, col_h - 2*wall, thickness - floor - ceiling, centered=True)
     .translate((0, 0, (floor - ceiling)/2))  # keep a floor and a top wall
 )
-
+# hollow body with wall, floor and ceilings
 body = outer.cut(inner)
 
 
@@ -51,15 +52,17 @@ for i in range(keys):
 
 # -------- Angled sides --------
 
-# make a triangular prism, and add it to the right groove side of the body
-#prism = cq.Workplane("front")
-#    .lineTo(2.0, 0)
-#    .lineTo(2.0, 1.0)
-#    .threePointArc((1.0, 1.5), (0.0, 1.0))
-#    .close()
-#    .extrude(0.25)
-
-#show_object(prism, name="prism")
+# make a triangular prism, and add it to the right side of the body. This way, each added column 
+# contributes to an overall curvature of the full keyboard.
+prism_displacement = math.tan(math.radians(column_angle_deg)) * thickness
+body = (body.faces("<Y").workplane()
+    .center(col_w/2, 0)
+    .moveTo(0, -thickness/2)
+    .lineTo(0, thickness/2)
+    .lineTo(prism_displacement, thickness/2)
+    .close()
+    .extrude(-col_h)
+)
 
 # -------- Joining geometry (T socket and groove) --------
 
@@ -69,7 +72,10 @@ def make_tsocket(clearance):
     cutout = cq.Workplane("XY").box(join_indent + clearance, col_h, join_indent + clearance, centered=True)
     tsocket = tsocket.cut(cutout.translate((-join_depth/2 + join_indent/2, 0, join_height/2 - join_indent/2)))
     return tsocket.cut(cutout.translate((-join_depth/2 + join_indent/2, 0, -join_height/2 + join_indent/2)))
-pos_tsocket = make_tsocket(join_clearance).translate((col_w/2 + join_depth/2, 0, 0))
+pos_tsocket = (make_tsocket(join_clearance)
+    .rotate((0, 0, 0), (0, 1, 0), column_angle_deg)
+    .translate((col_w/2 + join_depth/2 + prism_displacement/2, 0, 0))
+)
 body = body.union(pos_tsocket)
 
 # Groove on the left edge
@@ -78,7 +84,14 @@ neg_tsocket = neg_tsocket.translate((-col_w/2 + join_depth/2, 0, 0))
 body = body.cut(neg_tsocket)
 
 show_object(body, name="body")
-show_object(body.translate((col_w, 0, 0)), name="body2")
+for i in range(5):
+    body = (body
+        .rotate((-col_w/2, 0, 0), (-col_w/2, 1, 0), column_angle_deg)
+        .translate((col_w + prism_displacement/2, 0, 0))
+    )
+    show_object(
+        body, name="body"+str(i)
+    )
 
 
 # Optional exports:
